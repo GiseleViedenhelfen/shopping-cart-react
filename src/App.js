bdbe8923a4dc9cc5d3e1dc7e915fdf100d1e769b
Gisele <gisele.viedenhelfen@gmail.com>
2022-08-17 15:31:09 -0300

    estilização de cards para visualização

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import { Route } from 'react-router-dom';
 import Main from './components/index'
 
-function App() {
-  return (
-    <div className="App">
-    <Main />
-    </div>
-  );
+class App extends Component {
+  render() {
+
+    return (
+      <div className="App">
+        <Route exact path="/" component={ Main }/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/cards/Card.js
+++ src/components/cards/Card.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './card.css'
+
 class Card extends Component {
   constructor() {
     super();
@@ -10,20 +12,29 @@ class Card extends Component {
 
 componentDidMount() {
   const { date } = this.props;
-  console.log(date);
-      this.setState({ resultApi: date })
+  this.setState({ resultApi: date })
 }
   render() {
     const { resultApi } = this.state;
   
     return (
-    <div>
+    <div className="cards-container">
       {resultApi.length > 0 && resultApi.map((item) =>(
-        <ul key= {item.id}>
-         <li> <img src={item.thumbnail} alt={item.title}/></li>
-          <li>{item.title}</li>
-          <li>{`R$ ${item.price}`}</li>
+        <div key= { item.id } className="card"> 
+        <ul>
+         <li> <img src={ item.thumbnail } alt={ item.title }/></li>
+          <li>{ item.title }</li>
+          <li>{ `R$ ${item.price}` }</li>
+          <li>
+          <button 
+            type="button"
+            onClick={() => {}}
+          >
+            Adicionar ao Carrinho
+          </button>
+          </li>
         </ul>
+        </div>
       ))}
     </div>
     )
